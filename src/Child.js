e7c6da9d95e66f39ca451713e1704c515b45820b
Laraib_Saleem <laraibsaleem98@gmail.com>
2020-06-19 21:28:17 +0500

    Complete useContext and useReducer Basic Code.

---
+++ src/Child.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import counterContext from './CounterContext';
+
+function Child() {
+    let counterValue = useContext(counterContext)
+    console.log(counterValue)
+    return (
+        <div>
+            <h1>This is first Child i.e for counterContext</h1>
+
+            <h2>Value of 'counterValue' is {counterValue[0]}</h2>
+
+            <button onClick={()=> {counterValue[1](++counterValue[0])}} > Increment Context </button>
+
+        </div>
+    );
+}
+
+export default Child;
---
+++ src/Child2.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from 'react';
+import counterReducer from './CounterReducer';
+
+const Child2 = () => {
+
+    let [state, dispatch] = useReducer(counterReducer, 0)
+    console.log(state)
+    return (
+        <div>
+            <h1>This is second child i.e for counterReducer</h1>
+
+            <h2>Value of 'state' is {state}</h2>
+
+            <button onClick={() => dispatch('INCREMENT')} > Increment Reducer </button>
+
+        </div>
+    )
+
+}
+
+export default Child2;
